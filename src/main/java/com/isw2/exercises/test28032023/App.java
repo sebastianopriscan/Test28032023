2850b59423009278b3d2c4fe6fd29c56f0bb5fbd
sebastianopriscan <sebastianroberto.opriscan@gmail.com>
2023-03-28 12:45:46 +0200

    Source first commit

---
+++ src/main/java/com/isw2/exercises/test28032023/App.java
@@ -0,0 +1,13 @@
+package com.isw2.exercises.test28032023;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Once I revert to an egg, I must bury myself underground for three years. There my form matures." );
+    }
+}
---
+++ src/test/java/com/isw2/exercises/test28032023/AppTest.java
@@ -0,0 +1,20 @@
+package com.isw2.exercises.test28032023;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
